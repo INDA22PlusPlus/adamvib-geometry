3904cc0418d563fb58de84e7c31019d3034543d9
Adam Viberg <66957534+adam-viberg@users.noreply.github.com>
2022-12-14 20:17:06 +0100

    New almost working Point in Polygon

--- point-in-polygon.py
+++ point-in-polygon.py
@@ -1,27 +1,14 @@
 # https://kth.kattis.com/problems/pointinpolygon?editsubmit=10115406
 
+import itertools
 import sys
-
-import numpy as np
 
 
 def left(a, b, p):
-    return (b[0] - a[0]) * (p[1] - a[1]) - (b[1] - a[1]) * (p[0] - a[0])
+    return (b[0] - a[0]) * (p[1] - a[1]) - (b[1] - a[1]) * (p[0] - a[0]) > 0
 
-def distance(a, b, p):
-    a = np.array(a)
-    b = np.array(b)
-    p = np.array(p)
-    return np.linalg.norm(np.cross(b-a, a-p))/np.linalg.norm(b-a)
-
-def counter_clockwise(polygon):
-    area = 0
-    for i in range(len(polygon)):
-        area += polygon[i][0] * polygon[(i + 1) % len(polygon)][1] - polygon[(i + 1) % len(polygon)][0] * polygon[i][1]
-    return area > 0
-
-def in_range(x, y, closest_line):
-    return (closest_line[0][0] <= x <= closest_line[1][0] or closest_line[1][0] <= x <= closest_line[0][0]) and (closest_line[0][1] <= y <= closest_line[1][1] or closest_line[1][1] <= y <= closest_line[0][1])
+def on(a, b, p):
+    return (b[0] - a[0]) * (p[1] - a[1]) - (b[1] - a[1]) * (p[0] - a[0]) == 0
 
 def main():
     while True:
@@ -35,30 +22,31 @@ def main():
             x, y = map(int, sys.stdin.readline().split())
             polygon.append((x, y))
         
-        cc = counter_clockwise(polygon)
         m = int(sys.stdin.readline())
-
         for _ in range(m):
-            x, y = map(float, sys.stdin.readline().split())
-            closest_line = ()
-
-            min_distance = float('inf')
-            for i in range(n-1):
-                distance = abs(left(polygon[i], polygon[i+1], (x, y)))
-                if distance < min_distance:
-                    min_distance = distance
-                    closest_line = (polygon[i], polygon[i+1])
-
-            left_d = left(closest_line[0], closest_line[1], (x, y))
+            x, y = map(int, sys.stdin.readline().split())
+            point = (x, y)
+            far_right_point = (10000, point[1])
             
-            if cc and left_d > 0 and in_range(x, y, closest_line):
-                print('in')
-            elif not cc and left_d < 0 and in_range(x, y, closest_line):
-                print('in')
-            elif left_d == 0 and in_range(x, y, closest_line):
+            is_on = False
+            for i in range(n-1):
+                if on(polygon[i], polygon[i+1], point) and min(polygon[i][0], polygon[i+1][0]) <= point[0] <= max(polygon[i][0], polygon[i+1][0]) and min(polygon[i][1], polygon[i+1][1]) <= point[1] <= max(polygon[i][1], polygon[i+1][1]):
+                    is_on = True
+                    break
+            if is_on:
                 print('on')
-            else:
+                continue
+
+            intersections = 0
+            for i, z in zip(range(n), itertools.chain(range(1, n), [0])):
+                if left(point, far_right_point, polygon[i]) != left(point, far_right_point, polygon[z]) and left(polygon[i], polygon[z], point) != left(polygon[i], polygon[z], far_right_point):
+                    if left(polygon[i], polygon[z], point) != left(polygon[i], polygon[z], far_right_point):
+                        intersections += 1
+
+            if intersections % 2 == 0:
                 print('out')
+            else:
+                print('in')
 
 if __name__ == '__main__':
     main()
